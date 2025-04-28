c167d163718f62952fef65a2296d9e1977b137c9
Roy-Kolkata <roy2004akash@gmail.com>
2025-04-28 23:35:18 +0530

    Add files via upload

---
+++ BufferdImage.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author USER
+ */
+class BufferdImage {
+
+    Image getScaledInstance(int newWidth, int newHeight, int SCALE_SMOOTH) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
